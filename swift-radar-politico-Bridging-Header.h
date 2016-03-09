394f3167fd821ccf6be4664b90606af07b744e79
ulysses <ulysses@student.dei.uc.pt>
2016-03-09 14:46:47 -0300

    objc/swift bridge

---
+++ swift-radar-politico-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  swift-radar-politico-Bridging-Header.h
+//  swift-radar-político
+//
+//  Created by Ulysses on 3/9/16.
+//  Copyright © 2016 Francisco José A. C. Souza. All rights reserved.
+//
+
+#ifndef swift_radar_politico_Bridging_Header_h
+#define swift_radar_politico_Bridging_Header_h
+
+
+#endif /* swift_radar_politico_Bridging_Header_h */
+
+#import "URLManager.h"
